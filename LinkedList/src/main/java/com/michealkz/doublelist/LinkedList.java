2d390d237047f014093be03d86c08e21250a8f41
NiceCloudCode <nicecloudcode@github.com>
2021-01-19 23:00:46 +0800

    双向链表add() clear()  indexOf() 操作实现

--- LinkedList/src/main/java/com/michealkz/doublelist/LinkedList.java
+++ LinkedList/src/main/java/com/michealkz/doublelist/LinkedList.java
@@ -32,19 +32,80 @@ public class LinkedList<E> extends AbstractList<E> {
     }
 
     public E set(int index, E element) {
-        return null;
+        Node<E> node = node(index);
+        E oldElement = node.element;
+        node.element = element;
+        return oldElement;
     }
 
+    /**
+     * 双向链表的添加操作
+     * 重点判断添加的index 位于头节点和尾节点的时候
+     *
+     * @param index
+     * @param element
+     */
     public void add(int index, E element) {
-
+        rangeCheckForAdd(index);
+        if (index == size) {// 链表的最后面进行添加
+            Node<E> oldNode = last;
+            last = new Node<E>(oldNode, element, null);
+            if (oldNode == null) {
+                // 表示这个是链表添加的第一个元素
+                first = last;
+            } else {
+                oldNode.next = last;
+            }
+        } else {
+            Node<E> next = node(index);// 获取原来Index位置的元素
+            Node<E> prev = next.prev; // 原来index位置的前一个元素
+            Node<E> node = new Node<E>(prev, element, next); // 创建新的节点
+            if (prev == null) {
+                // 说明是添加到 index == 0 位置
+                first = node;
+            } else {
+                prev.next = node;
+            }
+        }
     }
 
     public E remove(int index) {
-        return null;
+        rangeCheck(index);
+        Node<E> oldNode = node(index);
+        Node<E> prev = oldNode.prev;
+        Node<E> next = oldNode.next;
+
+        if (prev == null) {
+            // 说明移除的元素是首个元素
+            first = next;
+        } else {
+            prev.next = next;
+        }
+
+        if (next == null) {// 说明移除的元素是链表最后一个元素
+            last = prev;
+        } else {
+            next.prev = prev;
+        }
+        size--;
+        return oldNode.element;
     }
 
     public int indexOf(E element) {
-        return 0;
+        if (element == null) { // element 分为 null 和 值的判断
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (node.element == null) return i;
+                node = node.next;
+            }
+        } else {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (node.element.equals(element)) return i;
+                node = node.next;
+            }
+        }
+        return ELEMENT_NOT_FOUND;
     }
 
     /**
@@ -55,14 +116,14 @@ public class LinkedList<E> extends AbstractList<E> {
      */
     private Node<E> node(int index) {
         rangeCheck(index);
-
-        if (index < (size >> 1)) {
+        // 由于是双向链表，故而用中间元素作为判断，这里用位运算实现1/2的效果
+        if (index < (size >> 1)) {// 如果 index 小于 size/2 ，那么从头节点开始找
             Node<E> node = first;
             for (int i = 0; i < index; i++) {
                 node = node.next;
             }
             return node;
-        } else {
+        } else {// 如果index 大于 size/2 ,那么从尾节点开始找
             Node<E> node = last;
             for (int i = size - 1; i > index; i--) {
                 node = node.prev;
